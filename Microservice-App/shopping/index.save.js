f90ff9fafe016a8221c265daee127bec075b7f4b
shailendraya <shailendraya1988@gmail.com>
2023-03-15 12:29:25 +0530

    Initial microservice project setup with express-http-proxy module

--- Microservice-App/shopping/index.save.js
+++ Microservice-App/shopping/index.save.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const cors = require('cors');
-const proxy = require('express-http-proxy')
 
 const app = express();
 
---
+++ Microservice-App/shopping/src/config/index.js
@@ -0,0 +1,22 @@
+const dotEnv = require("dotenv");
+
+if (process.env.NODE_ENV !== "prod") {
+  const configFile = `./.env.${process.env.NODE_ENV}`;
+  dotEnv.config({ path: configFile });
+} else {
+  dotEnv.config();
+}
+console.log('Database connections---->', process.env.PORT, process.env.MONGODB_URI, process.env.APP_SECRET)
+
+const APP_SECRET ='jg_youtube_tutorial'
+
+// # Mongo DB
+const MONGODB_URI='mongodb://localhost:27017/amazon_demo'
+
+// # Port
+const PORT=8000
+module.exports = {
+  PORT: PORT,
+  DB_URL: MONGODB_URI,
+  APP_SECRET: APP_SECRET,
+};
